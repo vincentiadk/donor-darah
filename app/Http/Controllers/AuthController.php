138db20c353785cb1b5b27cf0e0eae8f6769ab0c
vincentiadk <vincentiadksitanggang@gmail.com>
2021-07-30 14:50:04 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Donor;
 use App\Models\Log;
 use App\Models\User;
-use App\Models\Donor;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -32,10 +32,8 @@ class AuthController extends Controller
                             $time = now();
                             session([
                                 'id' => $user->id,
-                                'username' => $user->username,
                                 'role_id' => $user->role_id,
                                 'email' => $user->email,
-                                'name' => $user->name,
                                 'last_login' => $time,
                             ]);
                             $user->update([
@@ -84,62 +82,73 @@ class AuthController extends Controller
 
     public function registration()
     {
-        if(request()->has('_token')) {
+        if (request()->has('_token')) {
             $validator = Validator::make(request()->all(), [
-                    'nama_ktp' =>"required",
-                    'email'     => 'required|email|unique:users,email',
-                    'whatsapp'  =>'required|numeric|unique:donors,whatsapp',
-                    'gol_darah' => 'required',
-                    'rhesus'    => 'required',
-                    'password'  =>  'confirmed:password_confirmed|required|min:6'
-                ], [
-                    'nama_ktp.required'  => 'Nama wajib diisi!',
-                    'email.required'     => 'Email wajib diisi!',
-                    'email.unique'       => 'Email Telah Terdaftar!',
-                    'password.required'  => 'Password wajib diisi!',
-                    'whatsapp.required' => 'Nomor WhatsApp wajib diisi!',
-                    'whatsapp.numeric'  => 'Nomor WhatsApp harus berupa angka!',
-                    'whatsapp.unique'   => "Nomor Whatsapp sudah terdaftar!",
-                    'gol_darah.required' => 'Golongan darah wajib diisi!',
-                    'rhesus.required'    => 'Rhesus wajib diisi!',
-                    'password.confirmed'   => 'Password konfirmasi harus sama dengan password awal!',
-                    'password.required' => 'Password harus diisi!',
-                    'password.min'  => 'Password minimal terdiri dari 6 karakter!'
-                ]);
-            if($validator->fails()) {
+                'nama_ktp' => "required",
+                'email' => 'required|email|unique:users,email',
+                'whatsapp' => 'required|numeric|unique:donors,whatsapp',
+                'gol_darah' => 'required',
+                'rhesus' => 'required',
+                'password' => 'confirmed|required|min:6',
+            ], [
+                'nama_ktp.required' => 'Nama wajib diisi!',
+                'email.required' => 'Email wajib diisi!',
+                'email.unique' => 'Email Telah Terdaftar!',
+                'password.required' => 'Password wajib diisi!',
+                'whatsapp.required' => 'Nomor WhatsApp wajib diisi!',
+                'whatsapp.numeric' => 'Nomor WhatsApp harus berupa angka!',
+                'whatsapp.unique' => "Nomor Whatsapp sudah terdaftar!",
+                'gol_darah.required' => 'Golongan darah wajib diisi!',
+                'rhesus.required' => 'Rhesus wajib diisi!',
+                'password.confirmed' => 'Password konfirmasi harus sama dengan password awal!',
+                'password.required' => 'Password harus diisi!',
+                'password.min' => 'Password minimal terdiri dari 6 karakter!',
+            ]);
+            if ($validator->fails()) {
                 $response = [
                     'status' => 422,
-                    'error'  => $validator->errors()
+                    'error' => $validator->errors(),
                 ];
                 return redirect()
-                ->back()
-                ->withInput()
-                ->withErrors($validator);
-                //return response()->json($response);
+                    ->back()
+                    ->withInput()
+                    ->withErrors($validator);
             }
             try {
                 $donor = Donor::create([
-                    'nama_ktp'          => request('nama_ktp'),
-                    'nama_panggilan'    => count(explode(' ',request('nama_ktp'))) > 0 ? explode(' ', request('nama_ktp'))[0] : request('nama_ktp'),
-                    'gol_darah'     => request('gol_darah'),
-                    'rhesus'        => request('rhesus'),
-                    'whatsapp'      => request('whatsapp')
+                    'nama_ktp' => request('nama_ktp'),
+                    'nama_panggilan' => count(explode(' ', request('nama_ktp'))) > 0 ? explode(' ', request('nama_ktp'))[0] : request('nama_ktp'),
+                    'gol_darah' => request('gol_darah'),
+                    'rhesus' => request('rhesus'),
+                    'whatsapp' => request('whatsapp'),
                 ]);
                 $user = User::create([
-                    'email'     => request('email'),
-                    'password'  => Hash::make(request('password')),
-                    'userable_id'   => $donor->id,
-                    'role_id'   => 2,
-                    'email_verified_at' => date('Y-m-d')
+                    'email' => request('email'),
+                    'password' => Hash::make(request('password')),
+                    'userable_id' => $donor->id,
+                    'role_id' => 2,
+                    'email_verified_at' => date('Y-m-d'),
                 ]);
+                $time = now();
+                session([
+                    'id' => $user->id,
+                    'role_id' => $user->role_id,
+                    'email' => $user->email,
+                    'last_login' => $time,
+                ]);
+                $user->update([
+                    'last_login' => $time,
+                ]);
+                Log::create([
+                    'user_id' => session('id'),
+                    'activity' => 'login',
+                ]);
+                return redirect('admin/dashboard');
             } catch (\Exception $e) {
-                session()->flash('failed', 'Gagal daftar!');
-                $response = [
-                    'status'  => 500,
-                    'message' => 'Gagal daftar'
-                ];
+                return redirect()->back()->with([
+                    'failed' => 'Server Error, gagal daftar!',
+                ]);
             }
-            return redirect()->back();
 
         } else {
             return view('register');
