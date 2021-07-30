ed5f440eea854d2e00e5656114f2979cef9a04bf
tripurantoko <tripurantoko@gmail.com>
2021-07-30 10:16:37 +0700

    Ubah footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2021 <a href="https://darah.id">darah.id</a>.</strong> All rights reserved.
+    <strong>Hak Cipta &copy; 2021 <a href="https://darah.id">darah.id</a>.</strong> Seluruh kekayaan intelektual dilindungi undang-undang.
 </footer>
 
 <!-- Control Sidebar -->
