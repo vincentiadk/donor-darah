d9a81b8beae1fd7bcafc5f7c2457385ae8a70845
vincentiadk <vincentiadksitanggang@gmail.com>
2021-08-16 15:08:30 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Helper\Helper;
-use App\Models\Donor;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
